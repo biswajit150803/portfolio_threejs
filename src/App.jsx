1b810335fc5c0fd3653ac87778edde97084dbeec
Biswajit Ghosh <biswajitghosh150803@gmail.com>
2023-03-05 19:16:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from "./components"
-import Footer from './components/Footer';
+import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from "./components";
 function App() {
   return (
     <BrowserRouter>
